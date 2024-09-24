596bdde08b4f4db8c1f74f9352cfc2604fe5c82d
Bearbrand-sy <sanderperejan@gmail.com>
2024-09-24 13:52:23 +0800

    added loops  in app.js

--- app.js
+++ app.js
@@ -101,3 +101,24 @@ console.log(impression);
         break;
   } 
     console.log(todaysGreeting);
+
+//D. Loops
+//for loop
+
+for(let i=0; i < 5; i++ ){
+    console.log("The numbers are: " + i);
+}
+
+//while loop 
+let loop = 0;
+   while(loop < 6){
+    loop++;
+   }
+console.log("The numbers are: " + loop);
+
+//do while loop
+let s = 1;
+ do {
+    console.log("The numbers are: " + s);
+    s++;
+ }while (s < 5);
