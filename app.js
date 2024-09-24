cb14d935d0539f9e3b228faa28cf690f2e52bda7
Bearbrand-sy <sanderperejan@gmail.com>
2024-09-24 13:51:17 +0800

    added arithmetic and logical operators in app.js

--- app.js
+++ app.js
@@ -13,3 +13,36 @@ let surname;
 
 //null
 let firstname = null;
+
+//B. Arithmetic and logical operators
+// Arithmetic
+let x = 5;
+let y = 10;
+
+let sum = x + y;
+let diff = x - y;
+let prod = x * y;
+let qou = x / y;
+let mod = x % y;
+
+console.log("the sum is " + sum );
+console.log("the difference is " + diff );
+console.log("the product is " + prod );
+console.log("the qoutient is " + qou );
+console.log("the modulo is " + mod );
+
+// logical
+let num1 = 2;
+let num2 = 4;
+
+let result1 = num1 += 10;
+let result2 = num1 -= 10;
+let result3 = num1 *= 10;
+let result4 = num1 /= 10;
+let result5 = num1 %= 10;
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
