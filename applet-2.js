a3d385aeeb0045b13ae881deae6d57db7b458245
sanderperejan@gmailc.om <sanderperejan@gmail.com>
2024-09-27 19:43:55 +0700

    added initTileLayer method and syntax from leafletjs.com

--- applet-2.js
+++ applet-2.js
@@ -1,5 +1,12 @@
 class leafLetMap{
     constructor(containerId,center,zoom){
         this.map = L.map(containerId).setView(center,zoom);
+        this.initTileLayer();
+    }
+
+    initTileLayer(){
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
     }
 }
