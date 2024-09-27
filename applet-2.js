b31c59e11d3b75c43e05ed6c5fe5bb345719336e
sanderperejan@gmailc.om <sanderperejan@gmail.com>
2024-09-27 19:46:11 +0700

    added maxZoom in initTileLayer method

--- applet-2.js
+++ applet-2.js
@@ -6,7 +6,9 @@ class leafLetMap{
 
     initTileLayer(){
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Sample from new corales'
         }).addTo(this.map);
     }
+
 }
