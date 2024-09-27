774c2d6f429e1e3ef95f9fc839be7851293dd21e
sanderperejan@gmailc.om <sanderperejan@gmail.com>
2024-09-27 19:54:16 +0700

    added leaflet library

--- applet-2.js
+++ applet-2.js
@@ -1,14 +1,18 @@
-class leafLetMap{
-    constructor(containerId,center,zoom){
-        this.map = L.map(containerId).setView(center,zoom);
+class LeafletMap {
+    constructor(containerId, center, zoom) {
+        this.map = L.map(containerId).setView(center, zoom);
         this.initTileLayer();
     }
 
-    initTileLayer(){
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    initTileLayer() {
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Sample from new corales'
         }).addTo(this.map);
+    }
+
+    addMarker(lat, long, message) {
+
     }
 
 }
