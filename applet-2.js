9d8f685ae2b5b9fdef927110dc9df7f174825e9c
sanderperejan@gmailc.om <sanderperejan@gmail.com>
2024-09-27 21:07:35 +0700

    added loadMarkersFromJson method

--- applet-2.js
+++ applet-2.js
@@ -17,6 +17,17 @@ class LeafletMap {
         .bindPopup(message);
     }
 
+    loadMarkersFromJson(url) {
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(marker => {
+                this.addMarker(marker.latitude, marker.longitude, marker.message);
+            });
+        })
+        .catch(error => console.error("Error Loading servers:", error));
+    }
+    
 
 }
 const Mymap = new LeafletMap('map', [8.371400, 124.855103], 13);
