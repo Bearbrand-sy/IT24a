cde9620ce73b853909d371e00268ceacb0b22d88
Bearbrand-sy <sanderperejan@gmail.com>
2024-10-01 11:22:51 +0800

    added constructor for class DataLogger and modified logCount into id in applet-3.html

--- applet-3.js
+++ applet-3.js
@@ -4,12 +4,8 @@ class DataLogger{
         this.clearButton = document.getElementById(clearButtonId);
         this.idContainer = document.getElementById(cardContainerId);
         this.logCountElement = document.getElementById(logCountId);
-        this.loggedData = [];
+        this.loggedData = [];     
 
-        this.logButton = addEventListener('click', () => this.logData());
-
-
-
-
+        
     }
 }
