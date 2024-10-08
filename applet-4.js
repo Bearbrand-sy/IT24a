afad928e930e9d4cc2bccee9269bf8ebe988ee1f
Bearbrand-sy <sanderperejan@gmail.com>
2024-10-08 10:45:40 +0800

    added parameters in init method

--- applet-4.js
+++ applet-4.js
@@ -2,14 +2,17 @@ class studentList{
     constructor(dataUrl){
            this.dataUrl = dataUrl;
            this.students = [];
+           this.init();
     }
     async init(){
-           
+           await this.fetchData();
+           this.renderStudentList(this.students);
     }
     async fetchData(){
-
+       
     }
     renderStudentList(students){
-        
+
     }
+
 }
